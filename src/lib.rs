f0a947507c3350532ab88575755144c12ef80c38
WatzTheLeaf <louistheleaf@pm.me>
2026-02-20 20:08:07 +0100

    improve exposed function by adding checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+use std::io::Error;
+use std::io::ErrorKind::InvalidInput;
+use std::path::Path;
 use crate::save::save;
 
 mod save;
 
 pub fn export_inputs(path: &str, raw_inputs: Vec<u8>, rate: u8) -> std::io::Result<()> {
+    let path = Path::new(path);
+    if path.extension().map_or(true, |ext| ext != "dat") {
+        return Err(Error::new(InvalidInput, "Invalid file format given"))
+    }
     save(path.as_ref(), &raw_inputs, rate)
 }
