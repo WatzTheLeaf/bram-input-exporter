782824beee3d8a38c27929615604c57bdf91a51f
WatzTheLeaf <louistheleaf@pm.me>
2026-02-24 19:05:40 +0100

    fix header

--- src/save.rs
+++ src/save.rs
@@ -5,7 +5,7 @@ use byteorder::{LittleEndian, WriteBytesExt};
 
 pub(crate) fn save(path: &Path, data: &[u8], rate: u8) -> std::io::Result<()> {
     let mut file = BufWriter::new(File::create(path)?);
-    file.write_all(b"BRAM-INPUT-DATA")?;
+    file.write_all(b"BRAMIND")?;
     file.write_u8(rate)?;
     file.write_u64::<LittleEndian>(data.len() as u64)?;
     file.write_all(data)?;
